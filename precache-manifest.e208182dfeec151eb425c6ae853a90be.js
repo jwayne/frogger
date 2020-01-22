741891264809749eb6ab5e66873fbd140efc315a
Josh Chen <jwayne@users.noreply.github.com>
2020-01-22 17:47:06 -0500

    Updates

---
+++ precache-manifest.e208182dfeec151eb425c6ae853a90be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73bac84589eddb2a04cb1112b002a939",
+    "url": "/frogger/index.html"
+  },
+  {
+    "revision": "a8e7f53315203d1857c4",
+    "url": "/frogger/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "48e4ac3bf6d5b736ad18",
+    "url": "/frogger/static/js/2.300dfc9c.chunk.js"
+  },
+  {
+    "revision": "1eed39b55c0bb7c26ec629a63f87aa2a",
+    "url": "/frogger/static/js/2.300dfc9c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a8e7f53315203d1857c4",
+    "url": "/frogger/static/js/main.9d46e374.chunk.js"
+  },
+  {
+    "revision": "90f4ffa768892a56aece",
+    "url": "/frogger/static/js/runtime-main.beb97a03.js"
+  },
+  {
+    "revision": "d0763e0d0ed18c79ae2bafbaafc4bb50",
+    "url": "/frogger/static/media/frog.d0763e0d.png"
+  }
+]);
